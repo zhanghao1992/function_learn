8e1e2f8ce1f524d53a0ecf15b0d7d39845a52845
zhanghao23 <386708307@qq.com>
2020-02-19 18:07:44 +0800

    throlle

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -10,6 +10,7 @@ import styled from 'styled-components';
 import {getNews} from '../../request/index.js'
 // import data from './data.js'
 import a from './a.js'
+import './style.css'
 
 console.log(a)
 setTimeout(() => {
@@ -46,81 +47,52 @@ class Home  extends Component {
     super(props)
     this.state = {
       name: 'zhanghao23',
-      count: 0
+      count: 0,
+      instance: this
     }
   }
 
   componentDidMount() {
-    // setTimeout(() => {
-    //   this.setState({count: 2})
-    // }, 2000)
-    // const fn = () => {
-    //   return new Promise(resolve =>{
-    //       setTimeout(() => {
-    //         Promise.resolve(5)
-    //       }, 1000)
-    //   })
-    // }
-
-    // fn().then(res => {
-    //   console.log('11')
-    //     console.log(res)
+    // getNews().then(res => {
+    //   console.log(res);
     // })
-    getNews().then(res => {
-      console.log(res);
-    })
-    
-    document.getElementById('btn-confirm').addEventListener('click', function(e) {
-      // e.stopPropagation()
-      console.log('原生事件')
-    }, false)
-    
+  }
+
+  say = () => {
+    console.log('say');
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    prevState.instance.say()
+    console.log(prevState.instance, 'getDerivedStateFromProps');
+    return null
+
+  }
+
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate');
+    return true
   }
 
   componentWillUnmount() {
     console.log('componentWillUnmount')
   }
 
-  fetchD = () => {
-    console.log(this);
-    // const { getAllCities } = this.props
-    // getAllCities()
-  }
-      //测试函数
-  handler = (e) => {
-      console.log(e);
+  addFn = () => {
+    let { count } = this.state
+    this.setState({ 
+      count: count + 1
+    })
   }
 
-  abort = () => {
-    this.controller.abort();
-  }
-
-  handleClickWrap = (e) => {
-    // console.log('handleClickWrap');
-  }
-
-  handleClick =(e) => {
-    // e.nativeEvent.stopImmediatePropagation();
-    console.log('button click');
-  }
-  
-  handleNativeAndReact = (e) => {
-    console.log('原生+react 事件:  当前执行react事件');
-  }
-  
   render() {
-    const {loading} = this.props
+    // const {loading} = this.props
     return(
       <div>
         {this.state.count}
-        <Button loading={loading.effects['app/save']} onClick={this.fetchD}>发送请求</Button>
-        <input id = "btn" type="button" value='单击this' onClick={this.handler}/>
-
-        <div className="pageIndex" onClick={this.handleClickWrap}>
-          <p>react event!!!</p>
-            <MyButton id="btn-confirm" onClick={this.handleClick}>react 事件</MyButton>
-            <MyButton id="btn-reactandnative" primary onClick={this.handleNativeAndReact}>原生 + react 事件</MyButton>
-        </div>
+        <button onClick={this.addFn}>+</button>
+        <input placeholder="asdasd"/>
+        <div className="box">asdasda</div>
       </div>
     )
   }
--- src/views/Rx/index.js
+++ src/views/Rx/index.js
@@ -4,22 +4,96 @@
  * @Description: memoize和尾递归优化性能
  */
 import React, {Component} from 'react'
-import { of } from 'rxjs/observable/of'; 
-import { map, filter } from 'rxjs/operators';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/repeat';
+// import 'rxjs/add/observable/interval';
+
+// import { of } from 'rxjs/observable/of'; 
+// import { range } from 'rxjs/observable/range'; 
+// import { generate } from 'rxjs/observable/generate'; 
+import { 
+  Observable,
+   Subject,
+   asapScheduler,
+   pipe,
+   of,
+   from,
+   interval,
+   timer,
+   fromEvent,
+   concat,
+   merge,
+   zip,
+   range,
+   combineLatest,
+   race
+} from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { 
+  map,
+  mapTo,
+  withLatestFrom,
+  repeatWhen,
+  take,
+  concatAll,
+  mergeAll,
+  switchMap,
+  zipAll,
+  exhaust,
+  count,
+  max,
+  reduce,
+  filter,
+  first,
+  last,
+  takeLast,
+  takeUntil,
+  takeWhile,
+  skip,
+  skipWhile,
+  skipUntil,
+  skipLast,
+  throttleTime,
+  throttle,
+  debounceTime,
+  debounce,
+} from 'rxjs/operators';
 
 class Rx  extends Component {
   componentDidMount() {
-    const source$ = of(1, 2, 3); 
-    const result$ = source$.pipe( 
-        filter((x) => x % 2 === 0),
-        map(x => x * x) 
+    // const btn = document.querySelector('#btn')
+    // const source$ = fromEvent(btn, 'click')
+    const notifier = (notifica) => {
+      return interval(1000)
+    }
+    // const original$ = timer(0, 1000);
+    const source1$ = interval(500).pipe(
+      take(2),
+      mapTo('A')
+    )    
+    const source2$ = interval(1000).pipe(
+      take(3),
+      mapTo('B')
+    )    
+    const source3$ = interval(500).pipe(
+      take(3),
+      mapTo('C')
+    )
+ 
+    const result$ = concat(source1$, source2$, source3$).pipe(
+      throttleTime(800)
+    )
+    result$.subscribe(
+      console.log,
+      err => console.log(err),
+      console.log('complete')
     );
-    result$.subscribe(console.log);
   }
   render() {
     return(
       <div>
           Rx
+          <button id="btn">按钮</button>
       </div>
     )
   }

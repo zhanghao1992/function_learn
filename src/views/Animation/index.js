7880d8febd07a47af96c10f4f9007d30cebcfb03
zhanghao23 <386708307@qq.com>
2020-03-25 16:27:34 +0800

    transition

---
+++ src/views/Animation/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+const Animation = (props) => {
+
+  return (
+    <div>
+      <div className="box">中</div>
+    </div>
+  )
+}
+
+export default Animation
